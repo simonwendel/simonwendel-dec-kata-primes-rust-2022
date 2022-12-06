85c40f0b9588dfbcfcdf6eff8c37e321edf015b0
Simon Wendel <mail@simonwendel.se>
2022-12-06 17:31:14 +0100

    feat: optimize maximum size of n possible

--- src/primes.rs
+++ src/primes.rs
@@ -1,21 +1,22 @@
 fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
-    let mut all_integers = (0..=n).into_iter().map(Some).collect::<Vec<_>>();
-    all_integers[0] = None;
-    all_integers[1] = None;
+    let mut all_integers = (0..=n).into_iter().map(|_| true).collect::<Vec<_>>();
+    all_integers[0] = false;
+    all_integers[1] = false;
 
-    let max_factor = ((n as f32).sqrt() + 1.0) as usize;
+    let max_factor = ((n as f64).sqrt() + 1.0) as usize;
 
     for number in 2..=max_factor {
-        if all_integers[number].is_some() {
+        if all_integers[number] {
             let mut j = usize::pow(number, 2);
             while j <= n {
-                all_integers[j] = None;
+                all_integers[j] = false;
                 j += number;
             }
         }
     }
-
-    all_integers.into_iter().flatten().collect()
+    
+    let primes = all_integers.into_iter().enumerate().filter_map(|(i, val)| if val { Some(i) } else { None} );
+    primes.collect()
 }
 
 pub fn get_up_until(max_number: usize) -> Vec<usize> {
